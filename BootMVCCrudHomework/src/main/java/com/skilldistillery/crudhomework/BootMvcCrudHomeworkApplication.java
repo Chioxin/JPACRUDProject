3de21bd63d9e6802b85c7d1d880b5e437a179910
Greg Kraehenbuehl <chioxin@gmail.com>
2019-03-01 15:44:04 -0700

    Start Boot Project

---
+++ BootMVCCrudHomework/src/main/java/com/skilldistillery/crudhomework/BootMvcCrudHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.crudhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcCrudHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcCrudHomeworkApplication.class, args);
+	}
+
+}
---
+++ BootMVCCrudHomework/src/test/java/com/skilldistillery/crudhomework/BootMvcCrudHomeworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.crudhomework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootMvcCrudHomeworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
