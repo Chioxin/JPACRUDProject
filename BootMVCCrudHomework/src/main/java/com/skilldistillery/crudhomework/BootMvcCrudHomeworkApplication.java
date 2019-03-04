cc04b62f9dbe9bf7f6a9d62764949b39371f9361
Greg Kraehenbuehl <chioxin@gmail.com>
2019-03-04 08:56:21 -0700

    Configured app for tomcat deployment

--- BootMVCCrudHomework/src/main/java/com/skilldistillery/crudhomework/BootMvcCrudHomeworkApplication.java
+++ BootMVCCrudHomework/src/main/java/com/skilldistillery/crudhomework/BootMvcCrudHomeworkApplication.java
@@ -3,10 +3,16 @@ package com.skilldistillery.crudhomework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.skilldistillery.jpaproject")
-public class BootMvcCrudHomeworkApplication {
+public class BootMvcCrudHomeworkApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootMvcCrudHomeworkApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootMvcCrudHomeworkApplication.class, args);
